fa4c09c8c0b5d292f18f474f29cd1389d3165225
Health-Care-Step <71071988+Health-Care-Step@users.noreply.github.com>
2020-12-25 18:27:08 +0200

    Add files via upload

---
+++ precache-manifest.fc9aa6f49c2650ca216461f3dbc42391.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5060df67c1105916a6f1ba0bf118adb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a74504e0b11a09b4efb2",
+    "url": "/static/css/main.ed700820.chunk.css"
+  },
+  {
+    "revision": "b84a0ee39c237b786f91",
+    "url": "/static/js/2.99d3c95b.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.99d3c95b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a74504e0b11a09b4efb2",
+    "url": "/static/js/main.8b7b59d7.chunk.js"
+  },
+  {
+    "revision": "c7474c3876da4b5e14ab",
+    "url": "/static/js/runtime-main.bceafa69.js"
+  },
+  {
+    "revision": "0798a3774e43050a5384f8ba77653c92",
+    "url": "/static/media/RC-NEWS-Growing-Smarter-report-released-0219.0798a377.jpg"
+  },
+  {
+    "revision": "afce843d7cadad787b6d3848ff23609c",
+    "url": "/static/media/star-1.afce843d.svg"
+  },
+  {
+    "revision": "ca05b0338660aed37e25c6e4c6bbdd4b",
+    "url": "/static/media/star-2.ca05b033.svg"
+  }
+]);
